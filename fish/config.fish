ac9289c96279502e67bfef91eb78c8f317ee8f10
Gabe <gabeericwolf@gmail.com>
2019-07-25 11:52:20 -0400

    added some stuff for powerline

--- fish/config.fish
+++ fish/config.fish
@@ -71,7 +71,7 @@ end
 # --------------------------------------------------------------------
 
 function lessbin -d "open a text output of a .bin file in less" 
-    xxd "$1" | less 
+    xxd $argv[1] | less 
 end
 
 
@@ -93,57 +93,57 @@ end
 
 
 function gup-r -d "copy something to somewhere recursive"
-    aws s3 cp "$1" "$2" --recursive 
+    aws s3 cp $argv[1] $argv[2] --recursive 
 end
 
 function gup -d "copy something to somewhere "
-    aws s3 cp "$1" "$2" 
+    aws s3 cp $argv[1] $argv[2] 
 end
 
 function urec -d "upload something to somewhere on the ammobox lambda dev bucket recursively"
-    aws s3 cp "$1" s3://issacs-lambda-dev/Ammobox/IMU_Data/"$2"/ --recursive 
+    aws s3 cp $argv[1] s3://issacs-lambda-dev/Ammobox/IMU_Data/$argv[2]/ --recursive 
 end 
 
 function ufile -d "upload something to somewhere on the ammobox lambda dev bucket"
-    aws s3 cp "$1" s3://issacs-lambda-dev/Ammobox/IMU_Data/"$2"/ 
+    aws s3 cp $argv[1] s3://issacs-lambda-dev/Ammobox/IMU_Data/$argv[2]/ 
 end
 
 
 function vdate -d "view the contents of a date for the gpr bucket ammobox"
-    aws s3 ls s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-"$1"/ 
+    aws s3 ls s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-$argv[1]/ 
 end
 
 function vsdate -d "view the contents of a date for the gpr bucket smallbox"
-    aws s3 ls s3://issacs-gpr-dev/Smallbox/IMU_Data/DataLogs-"$1"/ 
+    aws s3 ls s3://issacs-gpr-dev/Smallbox/IMU_Data/DataLogs-$argv[1]/ 
 end
 
 function urdate -d "upload something to some date in the lambda bucket recursively"
-    aws s3 cp "$1" s3://issacs-lambda-dev/Ammobox/IMU_Data/DataLogs-"$2"/ --recursive 
+    aws s3 cp $argv[1] s3://issacs-lambda-dev/Ammobox/IMU_Data/DataLogs-$argv[2]/ --recursive 
 end
 
 function udate -d "upload something to some date in the lambda bucket"
-    aws s3 cp "$1" s3://issacs-lambda-dev/Ammobox/IMU_Data/DataLogs-"$2"/ 
+    aws s3 cp $argv[1] s3://issacs-lambda-dev/Ammobox/IMU_Data/DataLogs-$argv[2]/ 
 end 
 
 
 function ddate -d "download all the files of some date to somewhere recursively"
-    aws s3 cp s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-"$1"/ "$2" --recursive 
+    aws s3 cp s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-$argv[1]/ $argv[2] --recursive 
 end
 
 function dwdate -d "download the files from some date ammobox gpr bucket to the visualization folder for that same date on the host machine recursively"
-    aws s3 cp s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-"$1"/ /mnt/c/Users/gabee/Visualization/IMU\ Bin\ Files/"$1"/  --recursive 
+    aws s3 cp s3://issacs-gpr-dev/Ammobox/IMU_Data/DataLogs-$argv[1]/ /mnt/c/Users/gabee/Visualization/IMU\ Bin\ Files/$argv[1]/  --recursive 
 end
 
 function dswdate -d "download the files from some date smallbox gpr bucket to the visualization folder for that same date on the host machine recursively"
-    aws s3 cp s3://issacs-gpr-dev/Smallbox/IMU_Data/DataLogs-"$1"/ /mnt/c/Users/gabee/Visualization/IMU\ Bin\ Files/"$1"/  --recursive 
+    aws s3 cp s3://issacs-gpr-dev/Smallbox/IMU_Data/DataLogs-$argv[1]/ /mnt/c/Users/gabee/Visualization/IMU\ Bin\ Files/$argv[1]/  --recursive 
 end
  
 function dfile -d "download something to somewhere"
-    aws s3 cp "$1" "$2" 
+    aws s3 cp $argv[1] $argv[2] 
 end
 
 function drec -d "download something to somewhere recursively"
-    aws s3 cp "$1" "$2" --recursive 
+    aws s3 cp $argv[1] $argv[2] --recursive 
 end
 
 
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/gabe/.local/share/omf/themes/bobthefish/fish_prompt.fish
